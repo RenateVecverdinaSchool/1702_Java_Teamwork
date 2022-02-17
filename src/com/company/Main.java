9e0ac9fd6ca5687397df373753aeafd78c58d1c2
ReJurcis <99201217+ReJurcis@users.noreply.github.com>
2022-02-17 19:50:30 +0100

    Team work done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Renāte Vecvērdiņa");
-        System.out.println("Juris Upmanis");
+        System.out.println("Juris Upmanis-gkdfnl");
     }
 }
